d60ab8179b056669830e2dc8870dfeeeafef558d
Yuriy Deneha <yuriy.deneha@reyble.team>
2021-03-19 19:46:39 +0200

    Picker | added landing page; added dynamic width property

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  message = '';
+
+
+  addEmoji(symbol: string): void {
+    this.message += ` ${symbol} `;
+  }
 }
--- app/src/app/emoji-picker/emoji-picker.component.ts
+++ app/src/app/emoji-picker/emoji-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import * as emojiJSON from './data/list.json';
 
 @Component({
@@ -7,8 +7,9 @@ import * as emojiJSON from './data/list.json';
   styleUrls: ['./emoji-picker.component.scss']
 })
 export class EmojiPickerComponent {
+  @Input() width: string = '80vw';
+  isOpen: boolean;
   categories = [{"name":"Smileys & Emotion","cellsNumber":23},{"name":"People & Body","cellsNumber":16},{"name":"Animals & Nature","cellsNumber":18},{"name":"Food & Drink","cellsNumber":14},{"name":"Activities","cellsNumber":10},{"name":"Travel & Places","cellsNumber":33},{"name":"Objects","cellsNumber":34},{"name":"Symbols","cellsNumber":39},{"name":"Flags","cellsNumber":53}]
-
   emojiArr = emojiJSON['default'];
   @Output() onEmojiAdd = new EventEmitter();
   constructor() { }
@@ -19,6 +20,14 @@ export class EmojiPickerComponent {
 
   emojiIdentifier(item): string {
     return item.html;
+  }
+
+  togglePicker(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  hidePicker(): void {
+    this.isOpen = false
   }
 
   emitEmoji(event: MouseEvent, emoji: string): void {
